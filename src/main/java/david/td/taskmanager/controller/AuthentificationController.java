b7b332acf080e1ea0bf90e291b1166e0f2e3f0df
David Carvalho <david-michael.crvlh@eduge.ch>
2023-08-09 18:42:04 +0200

    feat: handled registration and login POST mappings

--- src/main/java/david/td/taskmanager/controller/AuthentificationController.java
+++ src/main/java/david/td/taskmanager/controller/AuthentificationController.java
@@ -1,11 +1,20 @@
 package david.td.taskmanager.controller;
 
+import david.td.taskmanager.model.User;
+import david.td.taskmanager.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AuthentificationController {
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login")
     public String login() {
@@ -13,7 +22,24 @@ public class AuthentificationController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String showRegisterPage() {
         return "register";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@RequestParam("username") String username, @RequestParam("password") String password, RedirectAttributes redirectAttributes) {
+        if (userService.authenticateUser(username,password)){
+            redirectAttributes.addFlashAttribute("message", "Nom d'utilisateur correct");
+            return "redirect:/main";
+        } else{
+            redirectAttributes.addFlashAttribute("error", "Nom d'utilisateur incorrect");
+            return "redirect:/login";
+        }
+    }
+
+    @PostMapping("/register")
+    public String processRegisterForm(@ModelAttribute("user") User user) {
+        userService.registerUser(user);
+        return "redirect:/login";
     }
 }
