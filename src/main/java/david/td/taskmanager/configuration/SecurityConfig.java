013118702c1bbf4d05a219b8773d7b0b76f4c720
David Carvalho <david-michael.crvlh@eduge.ch>
2023-08-09 18:46:21 +0200

    refactor: disabled spring security login page and instantiated BCryptPasswordEncoder

---
+++ src/main/java/david/td/taskmanager/configuration/SecurityConfig.java
@@ -0,0 +1,25 @@
+package david.td.taskmanager.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception
+    {
+        security.httpBasic().disable();
+        security.cors().and().csrf().disable();
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
