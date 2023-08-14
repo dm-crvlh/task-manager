7ebad8b10132886be1025e81394d4229c0757770
David Carvalho <david-michael.crvlh@eduge.ch>
2023-08-14 10:46:45 +0200

    feat: company selection added during user registration

--- src/main/java/david/td/taskmanager/controller/AuthentificationController.java
+++ src/main/java/david/td/taskmanager/controller/AuthentificationController.java
@@ -1,6 +1,9 @@
 package david.td.taskmanager.controller;
 
+import david.td.taskmanager.model.Company;
+import david.td.taskmanager.model.Project;
 import david.td.taskmanager.model.User;
+import david.td.taskmanager.repository.CompanyRepository;
 import david.td.taskmanager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -13,11 +16,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 public class AuthentificationController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @GetMapping("/login")
     public String login() {
@@ -25,7 +33,9 @@ public class AuthentificationController {
     }
 
     @GetMapping("/register")
-    public String showRegisterPage() {
+    public String showRegisterPage(Model model) {
+        List<Company> companies = companyRepository.findAll();
+        model.addAttribute("companies", companies);
         return "register";
     }
 
@@ -40,8 +50,8 @@ public class AuthentificationController {
     }
 
     @PostMapping("/register")
-    public String processRegisterForm(@ModelAttribute("user") User user) {
-        userService.registerUser(user);
+    public String processRegisterForm(@ModelAttribute("user") User user, @RequestParam("companyId") Long company) {
+        userService.registerUser(user, company);
         return "redirect:/login";
     }
 
