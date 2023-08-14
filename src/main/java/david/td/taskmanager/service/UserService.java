94d3fb3900eb2dbf6a98253eed81a4c0f8643453
David Carvalho <david-michael.crvlh@eduge.ch>
2023-08-14 17:03:14 +0200

    refactor: UserServiceImpl class deleted and transformed UserService interface into a class

--- src/main/java/david/td/taskmanager/service/UserService.java
+++ src/main/java/david/td/taskmanager/service/UserService.java
@@ -1,9 +1,74 @@
 package david.td.taskmanager.service;
 
+import david.td.taskmanager.model.Company;
+import david.td.taskmanager.model.Role;
 import david.td.taskmanager.model.User;
+import david.td.taskmanager.repository.CompanyRepository;
+import david.td.taskmanager.repository.RoleRepository;
+import david.td.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public interface UserService {
-    void registerUser(User user, Long selectedCompanyId);
-    User findByUsername(String username);
-    boolean authenticateUser(String username, String password);
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public void registerUser(User user, Long selectedCompanyId) {
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+
+        Role userRole = roleRepository.findByName("USER");
+        if (userRole == null) {
+            userRole = new Role();
+            userRole.setName("USER");
+            roleRepository.save(userRole);
+        }
+        user.setRoles(Collections.singletonList(userRole));
+        Company company = companyRepository.findById(selectedCompanyId).orElse(null);
+        if (company != null) {
+            user.setCompany(company);
+        }
+        System.out.println(user.getRoles().get(0).getName());
+        userRepository.save(user);
+    }
+
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public boolean authenticateUser(String username, String password) {
+        User user = findByUsername(username);
+        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            for (Role role : user.getRoles()) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+            }
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(username, password, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            return true;
+        }
+        return false;
+    }
 }
